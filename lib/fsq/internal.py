f233e49fec777f25b190af80b1e33b07778fbb85
Matthew Story <matt.story@axialmarket.com>
2012-07-03 19:01:14 -0400

    a few types we want to coerce to unicode, float and int, maybe more later, but for now that's it
    Signed-off-by: Matthew Story <matt.story@axialmarket.com>

--- lib/fsq/internal.py
+++ lib/fsq/internal.py
@@ -7,10 +7,20 @@
 import errno
 from . import FSQCoerceError
 
+_COERCE_THESE_TOO = (int,float,)
+
 def coerce_unicode(s, encoding='utf8'):
     if isinstance(s, unicode):
         return s
     try:
         return unicode(s, encoding=encoding)
-    except (TypeError, UnicodeEncodeError, UnicodeDecodeError, ), e:
+    except (UnicodeEncodeError, UnicodeDecodeError, ), e:
+        raise FSQCoerceError(errno.EINVAL, e.message)
+    except TypeError, e:
+        if s.__class__ in _COERCE_THESE_TOO:
+            try:
+                return unicode(s)
+            except Exception, e:
+                raise FSQCoerceError(errno.EINVAL, e.message)
+
         raise FSQCoerceError(errno.EINVAL, e.message)
