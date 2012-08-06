e9854bedc1db89299400509a873bb3d225f8f9de
Will O'Meara <will.omeara@axialmarket.com>
2012-08-06 15:02:04 -0400

    finishing up fsq down

--- libexec/fsq/down.py
+++ libexec/fsq/down.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# fsq-up(1) -- a program for turning on queues
+# fsq-down(1) -- a program for turning on queues
 #
 # @author: O'Meara will.omeara@axialmarket.com
 # @depends: fsq(1), fsq(7), python (>=2.7)
@@ -48,58 +48,19 @@ def usage(asked_for=0):
     sys.exit(exit)
 
 
-def done_item(item, code):
-    '''Succeed or fail an item based on the return code of a program'''
-    try:
-        if fsq.const('FSQ_SUCCESS') == code:
-            fsq.success(item)
-            chirp('{0}: succeeded'.format(item.id))
-        elif fsq.const('FSQ_FAIL_TMP') == code:
-            fsq.fail_tmp(item)
-            shout('{0}: failed temporarily'.format(item.id))
-        else:
-            fsq.fail_perm(item)
-            shout('{0}: failed permanantly'.format(item.id))
-    except fsq.FSQEnqueueError, e:
-        shout(e.strerror.encode(_CHARSET))
-        return -1
-    except fsq.FSQError, e:
-        shout(e.strerror.encode(_CHARSET))
-    return 0
-
-
-def setenv(item, timefmt):
-    '''Set environment, based on item.  Usually done in a baby fork'''
-    for env, att in (( 'FSQ_ITEM_PID', 'pid', ),
-                     ( 'FSQ_ITEM_ENTROPY', 'entropy', ),
-                     ( 'FSQ_ITEM_HOSTNAME', 'hostname', ),
-                     ( 'FSQ_ITEM_ID', 'id', ), ):
-        try:
-            os.putenv(env, getattr(item,
-                      att).encode(_CHARSET))
-        except UnicodeEncodeError:
-            shout('cannot coerce item {0};'
-                  ' charset={1}'.format(att, _CHARSET))
-            return -1
-
-    # format tries and date to env
-    os.putenv('FSQ_ITEM_TRIES', str(item.tries))
-    try:
-        os.putenv('FSQ_ITEM_ENQUEUED_AT',
-                  item.enqueued_at.strftime(timefmt))
-    except ValueError:
-        shout('invalid timefmt: {0}'.format(timefmt))
-        return -1
-    return 0
-
-
 # all fsq commands use a main function
 def main(argv):
     global _PROG, _VERBOSE
 
     _PROG = argv[0]
     try:
-        opts, args = getopt.getopt(argv[1:], 'hvd:', ( 'help', 'down-file'))
+        opts, args = getopt.getopt(argv[1:], 
+                                   'hvu:g:m:', ( 
+                                       'help', 
+                                       'verbose',
+                                       'user=',
+                                       'group=',
+                                       'mode=', ))
     except getopt.GetoptError, e:
         barf('invalid flag: -{0}{1}'.format('-' if 1 < len(e.opt) else '',
              e.opt))
@@ -122,7 +83,7 @@ def main(argv):
     try:
         for arg in args:
             fsq.down(arg, user, group, mode)
-            chirp('fsq up: {0}: up'.format(arg)) 
+            chirp('{0}: down'.format(arg)) 
     except (fsq.FSQScanError, fsq.FSQPathError, ), e:
         barf(e.strerror)
     except fsq.FSQCoerceError, e:
