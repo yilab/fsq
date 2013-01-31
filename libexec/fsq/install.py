21c6347aa46d7d43b79b3e5227a31ea0e37c9bf7
Matthew Story <matt.story@axialmarket.com>
2013-01-31 01:11:52 -0500

    the fsq install program, deprecates mkfsqueue
    Signed-off-by: Matthew Story <matt.story@axialmarket.com>
    Signed-off-by: Isaac (.ike) Levy <ike.levy@axialmarket.com>

---
+++ libexec/fsq/install.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# fsq-install(1) -- a program for installing an fsq queue.
+#
+# @author: Matthew Story <matt.story@axialmarket.com>
+# @depends: fsq(1), fsq(7), python (>=2.7)
+#
+# This software is for POSIX compliant systems only.
+import getopt
+import sys
+import fsq
+import os
+import errno
+
+_PROG = "fsq-install"
+_VERBOSE = False
+_CHARSET = fsq.const('FSQ_CHARSET')
+
+def shout(msg, f=sys.stderr):
+    '''Log to file (usually stderr), with progname: <log>'''
+    print >> f, "{0}: {1}".format(_PROG, msg)
+    f.flush()
+
+def usage(asked_for=0):
+    '''Exit with a usage string, used for bad argument or with -h'''
+    exit =  fsq.const('FSQ_SUCCESS') if asked_for else\
+                fsq.const('FSQ_FAIL_PERM')
+    f = sys.stdout if asked_for else sys.stderr
+    shout('{0} [opts] queue prog [args [...]]'.format(
+          os.path.basename(_PROG)), f)
+    if asked_for:
+        shout('{0} [-h|--help] [-v|--verbose] [-f|--force]'\
+              ' [-d|--down]'.format(os.path.basename(_PROG)), f)
+        shout('        [-t|--triggered] [-i|--ignore-down]', f)
+        shout('        [-o owner|--owner=user|uid]', f)
+        shout('        [-g group|--group=group|gid]', f)
+        shout('        [-m mode|--mode=int]', f)
+        shout('        queue [queue [...]]', f)
+    return 0 if asked_for else fsq.const('FSQ_FAIL_PERM')
+
+def main(argv):
+    global _PROG, _VERBOSE
+    force = False
+    is_down = False
+    is_triggered = False
+    user = None
+    group = None
+    mode = None
+    flag = None
+
+    _PROG = argv[0]
+    try:
+        opts, args = getopt.getopt(argv[1:], 'hvfdto:g:m:', ( '--help',
+                                   '--verbose', '--force', '--down',
+                                   '--triggered', '--owner', '--group',
+                                   '--mode', ))
+        for flag, opt in opts:
+            if flag in ( '-v', '--verbose', ):
+                _VERBOSE = True
+            elif flag in ( '-f', '--force', ):
+                force = True
+            elif flag in ( '-d', '--down', ):
+                is_down = True
+            elif flag in ( '-t', '--triggered', ):
+                is_triggered = True
+            elif flag in ( '-o', '--owner', ):
+                for c in ( 'FSQ_QUEUE_USER', 'FSQ_ITEM_USER', ):
+                    fsq.set_const(c, opt)
+            elif flag in ( '-g', '--group', ):
+                for c in ( 'FSQ_QUEUE_GROUP', 'FSQ_ITEM_GROUP', ):
+                    fsq.set_const(c, opt)
+            elif flag in ( '-m', '--mode', ):
+                for c in ( 'FSQ_QUEUE_MODE', 'FSQ_ITEM_MODE', ):
+                    fsq.set_const(c, opt)
+            elif flag in ( '-h', '--help', ):
+                return usage(1)
+
+        if 0 == len(args):
+            return usage()
+
+        for queue in args:
+            try:
+                fsq.install(queue, is_down=is_down, is_triggered=is_triggered)
+            except fsq.FSQInstallError, e:
+                if e.errno == errno.ENOTEMPTY and force:
+                    fsq.uninstall(queue)
+                    fsq.install(queue)
+                else:
+                    raise e
+
+    except ( fsq.FSQEnvError, fsq.FSQCoerceError, ):
+        shout('invalid argument for flag: {0}'.format(flag))
+        return fsq.const('FSQ_FAIL_PERM')
+    except fsq.FSQInstallError, e:
+        shout(e.strerror)
+        return fsq.const('FSQ_FAIL_TMP')
+    except getopt.GetoptError, e:
+        shout('invalid flag: -{0}{1}'.format('-' if 1 < len(e.opt) else '',
+              e.opt))
+        return fsq.const('FSQ_FAIL_TMP')
+
+if __name__ == '__main__':
+    main(sys.argv)
