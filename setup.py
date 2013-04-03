1168aa00f9624d841592ee1f746c328a966f833e
Isaac (.ike) Levy <ike.levy@axialmarket.com>
2013-04-03 14:02:44 -0400

    Version 0.2.1
    Signed-off-by: Isaac (.ike) Levy <ike.levy@axialmarket.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from glob import glob
 
 setup(
     name='fsq',
-    version='0.2.0',
+    version='0.2.1',
     author='Matthew Story',
     author_email='matt.story@axial.net',
     packages=['fsq', 'fsq.tests'],
